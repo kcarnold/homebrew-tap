325411cbe0c9a91684e9cd4ebb5fc21d2b95b60e
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2026-01-15 10:21:43 -0500

    bump version

--- Formula/mpc-bar.rb
+++ Formula/mpc-bar.rb
@@ -1,8 +1,8 @@
 class MpcBar < Formula
   desc "Menu bar client for the Music Player Daemon"
   homepage "https://github.com/kcarnold/mpc-bar"
-  url "https://github.com/kcarnold/mpc-bar/archive/refs/tags/v0.6.0.tar.gz"
-  sha256 "959ddb06bab083793877f4d9f2f5e644057bd22c4e5e4c9196ce4f541cc641ee"
+  url "https://github.com/kcarnold/mpc-bar/archive/refs/tags/v0.7.0.tar.gz"
+  sha256 "c574e4b21b9c860dc312aa385917f842ffaa5d74de49dbcaa400bf63a439633a"
   license "GPL-2.0-or-later"
   head "https://github.com/kcarnold/mpc-bar.git", branch: "main"
 
